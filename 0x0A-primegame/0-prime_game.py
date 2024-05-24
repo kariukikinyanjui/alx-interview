5673b6c9b102f990a41b68a38dfa6747b44aad8c
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-05-24 20:53:08 +0000

    [feat]: updated documentation

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -9,12 +9,6 @@ def isWinner(x, nums):
     def is_prime(n):
         '''
         Check if a number is prime
-
-        Parameters:
-        n (int): The number to check.
-
-        Returns:
-        bool: True if the number is prime, False otherwise.
         '''
         if n < 2:
             return False
